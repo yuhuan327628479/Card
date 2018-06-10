92743f4aed7e231d8f14f37afd07873f7ca8df4a
yuhuan327628479 <yuhuan327628479@qq.com>
2018-06-10 20:20:08 +0800

    处理跳转问题

--- src/pages/more.vue
+++ src/pages/more.vue
@@ -7,7 +7,7 @@
           <f7-list>
 
             <f7-list-item link="#" title="高级搜索"></f7-list-item>
-            <f7-list-item link="/form/"  view="#main-view" title="手动录入"></f7-list-item>
+            <f7-list-item link="/form/"  title="手动录入"></f7-list-item>
             <f7-list-item link="#" title="生日报表"></f7-list-item>
             <f7-list-item link="#" title="添加组"></f7-list-item>
             <f7-list-item link="#" title="已有分组"></f7-list-item>
